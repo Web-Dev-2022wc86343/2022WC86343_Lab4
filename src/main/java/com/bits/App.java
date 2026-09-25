e4aa094526b37790af74c830dc75a1be14939f59
Web-Dev-2022wc86343 <2022wc86343@wilp.bits-pilani.ac.in>
2026-09-25 11:49:48 +0530

    Create App.java

---
+++ src/main/java/com/bits/App.java
@@ -0,0 +1,11 @@
+package com.bits;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, BITS DevOps CI Pipeline!");
+    }
+
+    public String getStatus() {
+        return "SUCCESS";
+    }
+}
